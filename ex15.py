006dba5948e79c62dcafcfbfb1588d7f377be56d
Rafael Martins <102500171+rafaelcaetite@users.noreply.github.com>
2022-03-28 21:31:45 -0300

    Add files via upload

---
+++ ex15.py
@@ -0,0 +1,3 @@
+n1=float(input('Quantos km foram percorridos?'))
+n2=float(input('Por quantos dias o carro foi alugado?'))
+print(f'O valor total a ser pago pela locação é de {(60*n2)+(0.15*n1)} reais.')
